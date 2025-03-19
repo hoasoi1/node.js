960537ab94038a2b6532d05225e00909078f4f8f
huy533 <igonsakura@gmail.com.com>
2025-03-19 14:40:04 +0700

    node.js

---
+++ booking-system/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const bookingRoutes = require('./routes/bookingRoutes');
+
+const app = express();
+
+// Cấu hình Middleware
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+// Sử dụng routes
+app.use('/', bookingRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
